8e71ceabe8c9f9af2b916d16506a8003795fe260
George Shoemaker <george.a.shoemaker@gmail.com>
2020-04-14 14:45:07 -0600

    Use ejs to inject rootUrl

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 
-const app = express()
 const hostname = "127.0.0.1"
 const port = 3000
+const rootUrl = `http://${hostname}:${port}/`
+
+const app = express()
+app.set('view engine', 'ejs'); 
 
 app.get('/', (req, res) => res.send('Yeet !!') )
 app.get('/schnekk', (req, res) => {
-    res.sendFile('./public/snake.html', { root: __dirname } )
+    res.render('schnekk', {rootUrl: rootUrl}); 
 })
 
 app.use('/public', express.static('public'));
 
-app.listen(port, () => console.log(`App running at http://${hostname}:${port}/`) )
+app.listen(port, () => console.log(`App running at ${rootUrl}`) )
