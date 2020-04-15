a93538a3dbadc80f7cde3c2a0d82b9fa2369c2c6
George Shoemaker <george.a.shoemaker@gmail.com>
2020-04-14 21:10:35 -0600

    Use environment variables

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
-const hostname = "127.0.0.1"
-const port = 3000
+const hostname = process.env.HOST ? process.env.HOST : "127.0.0.1"
+const port     = process.env.PORT ? process.env.PORT : "3000"
 const rootUrl = `http://${hostname}:${port}/`
 
 const app = express()
