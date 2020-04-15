a103bf9b166a2f862e71b84b2c389e27b7c28992
George Shoemaker <george.a.shoemaker@gmail.com>
2020-04-14 22:12:14 -0600

    Add package* files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const hostname = process.env.HOST ? process.env.HOST : "127.0.0.1"
 const port     = process.env.PORT ? process.env.PORT : "3000"
-const rootUrl = `http://${hostname}:${port}/`
+const rootUrl  = `http://${hostname}:${port}/`
 
 const app = express()
 app.set('view engine', 'ejs'); 
