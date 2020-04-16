d51a0488d7251659838da72c2223f77310394182
George Shoemaker <george.a.shoemaker@gmail.com>
2020-04-16 16:12:31 -0600

    garbage 2

--- db/page-hits.js
+++ db/page-hits.js
@@ -1,66 +1,30 @@
 const sqlite3 = require('sqlite3').verbose()
 
-
-let connect = (filename, mode) => new Promise( (resolve, reject) => {
-    let db = new sqlite3.Database((filename, mode, err) => { if (err) {
-        reject(err.message)
-    }})
-    resolve("Db found")
+const connect = (filename) => new Promise( (resolve, reject) => {
+    let db = new sqlite3.Database(filename, sqlite3.OPEN_READWRITE, err => {
+        if (err) { reject(err) }
+    })
+    console.log("Connected")
+    resolve(db)
 })
 
+const initDb = (filename) => new Promise( (resolve, reject) => {
+    let db = new sqlite3.Database(filename, sqlite3.OPEN_CREATE, sqlite3.OPEN_READWRITE, err => {
+        if (err) reject(err)
+    })
+    resolve(db)
+})
 
-let checkMissingDb = (err) => {
-    if (err.code === 'SQLITE_CANTOPEN') return ("Initialized db")
-    return null
+const disconnect = (db) => {
+    db.close()
+    console.log("Closed")
 }
 
-connect('./db/page-hits.db', sqlite3.OPEN_READWRITE)
-// .catch(checkMissingDb)
-// .then(result => console.log(result))
+const checkMissingDb = err => {
+    console.log(Object.keys(err))
+}
 
-// new Promise((resolve, reject) => {
-//     console.log('Initial');
-
-//     resolve(1);
-// })
-// .then((n) => {
-//     throw new Error('Something failed');
-//     console.log('Do this');
-// })
-// .catch(() => {
-//     console.error('Do that');
-//     return 2
-// })
-// .then((n) => {
-//     console.log(n)
-//     console.log('Do this, no matter what happened before');
-// });
-
-// connect('./db/page-hits.js', sqlite3.OPEN_READWRITE)
-// .catch( err => {
-//     if (err.code === 'SQLITE_CANTOPEN') {
-//         // initializeDb
-//     }
-// })
-// .then()
-
-
-
-
-// let db = new sqlite3.Database('./db/page-hits.db', sqlite3.OPEN_READWRITE, (err) => {
-//     if (err){
-//         console.error(err.message)
-//         if (err.code === 'SQLITE_CANTOPEN') {
-//             console.log("Creating db for page-hits")
-//             createDb()
-//         }
-//         return
-//     }
-//     console.log('Connected to the page-hits database.');
-//   });
-
-//   const setupFilesystemDb = () => {
-//     let db = new sqlite3.Database('./db/page-hits.db');
-//     // TODO: add a table for page hits
-//   }
-  
+connect('fark')
+.catch()
+.then(disconnect)
+.catch(err => console.log(err.message))
